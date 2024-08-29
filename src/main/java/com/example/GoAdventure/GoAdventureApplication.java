d05645bfb7177110fe3b20fbd591b086af8d2990
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-08-30 01:01:15 +0300

    Created initial project structure.

---
+++ src/main/java/com/example/GoAdventure/GoAdventureApplication.java
@@ -0,0 +1,13 @@
+package com.example.GoAdventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoAdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoAdventureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GoAdventure/GoAdventureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GoAdventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoAdventureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
