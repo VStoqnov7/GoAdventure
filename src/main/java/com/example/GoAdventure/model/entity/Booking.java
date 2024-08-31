01eca5856ca5eb99368867fa27491d4a1bd33625
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-09-01 00:33:35 +0300

    Created initial project structure.

---
+++ src/main/java/com/example/GoAdventure/model/entity/Booking.java
@@ -0,0 +1,31 @@
+package com.example.GoAdventure.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@Entity
+@Table(name = "booking")
+public class Booking extends BaseEntity{
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Adventure adventure;
+
+    @Column(nullable = false, name = "booking_date")
+    private LocalDateTime bookingDate;
+
+    @Column(nullable = false, name = "registered_on")
+    private LocalDateTime registeredOn;
+}
---
+++ src/main/java/com/example/GoAdventure/model/entity/User.java
@@ -0,0 +1,28 @@
+package com.example.GoAdventure.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity{
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column
+    private String phone;
+}
