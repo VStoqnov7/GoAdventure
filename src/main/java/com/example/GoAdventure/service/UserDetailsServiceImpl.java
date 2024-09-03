0b255152da9cdd054da461eba81be197f239f17f
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-09-04 00:17:13 +0300

    Implemented UserDetailsService for customized user management.

---
+++ src/main/java/com/example/GoAdventure/service/UserDetailsServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.GoAdventure.service;
+
+import com.example.GoAdventure.model.entity.User;
+import com.example.GoAdventure.model.entity.UserRole;
+import com.example.GoAdventure.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByUsername(username)
+                .map(this::map)
+                .orElseThrow(() -> new UsernameNotFoundException("User with name " + username + " not found!"));
+    }
+
+    private UserDetails map(User user) {
+        if (user.isBanned()) {
+            throw new UsernameNotFoundException("User is banned.");
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream().map(this::mapRole).collect(Collectors.toList())
+        );
+    }
+
+    private GrantedAuthority mapRole(UserRole userRoleEntity) {
+        return new SimpleGrantedAuthority("ROLE_" + userRoleEntity.getName().name());
+    }
+}
