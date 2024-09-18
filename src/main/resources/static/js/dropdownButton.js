28e9c124586c33dacf556bf316dc08062214a60a
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-09-19 00:51:00 +0300

    Added dropdown toggle functionality for adventure category options.

---
+++ src/main/resources/static/js/dropdownButton.js
@@ -0,0 +1,14 @@
+<script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const toggleButton = document.getElementById('toggleOptions');
+            const optionsContainer = document.getElementById('optionsContainer');
+
+            toggleButton.addEventListener('click', function () {
+                if (optionsContainer.style.display === 'flex') {
+                    optionsContainer.style.display = 'none';
+                } else {
+                    optionsContainer.style.display = 'flex';
+                }
+            });
+        });
+</script>
