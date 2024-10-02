055cf239adbfeb0fef218ac55f5ff5ad89c85581
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-10-03 00:21:51 +0300

    Added ScrollReveal animations for home and adventures pages.

---
+++ src/main/resources/static/js/scroll-items.js
@@ -0,0 +1,34 @@
+ScrollReveal().reveal(".discover-card", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "50px",
+  interval: 500,
+});
+
+ScrollReveal().reveal(".destination-card", {
+  duration: 2000,
+  origin: "right",
+  distance: "50px",
+  interval: 300,
+});
+
+ScrollReveal().reveal(".socials", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "50px",
+  interval: 300,
+});
+
+ScrollReveal().reveal(".social-item", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "50px",
+  interval: 400,
+});
+
+ScrollReveal().reveal(".reveal-text", {
+    duration: 2000,
+    origin: "right",
+    distance: "100px",
+    interval: 400,
+});
