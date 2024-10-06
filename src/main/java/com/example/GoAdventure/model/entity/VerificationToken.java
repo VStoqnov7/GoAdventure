84c57a00b5da3c5e3d108ec9a79abc18cc907d3b
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-10-07 00:04:16 +0300

    Added VerificationToken entity for user registration verification.

---
+++ src/main/java/com/example/GoAdventure/model/entity/VerificationToken.java
@@ -0,0 +1,24 @@
+package com.example.GoAdventure.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@Entity
+@Table(name = "verification_token")
+public class VerificationToken extends BaseEntity{
+
+    private String token;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    private LocalDateTime expiryDate;
+}
