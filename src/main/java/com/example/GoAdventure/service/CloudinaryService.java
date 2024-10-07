15b786bd4a967a65af4e2213220954e069190291
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-10-08 00:07:33 +0300

    Added CloudinaryService for image upload functionality.

---
+++ src/main/java/com/example/GoAdventure/service/CloudinaryService.java
@@ -0,0 +1,47 @@
+package com.example.GoAdventure.service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class CloudinaryService {
+
+    private final Cloudinary cloudinary;
+
+    public CloudinaryService(@Value("${cloudinary.name}") String cloudName,
+                             @Value("${cloudinary.api-key}") String apiKey,
+                             @Value("${cloudinary.api-secret}") String apiSecret) {
+        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret,
+                "secure", true));
+    }
+
+    public String saveImage(MultipartFile multipartFile) {
+        String imageId = UUID.randomUUID().toString();
+        Map<String, Object> params = ObjectUtils.asMap(
+                "public_id", imageId,
+                "overwrite", true,
+                "resource_type", "image");
+
+        File tmpFile = new File(imageId);
+        try {
+            Files.write(tmpFile.toPath(), multipartFile.getBytes());
+            Map<String, Object> uploadResult = cloudinary.uploader().upload(tmpFile, params);
+            Files.delete(tmpFile.toPath());
+            return uploadResult.get("secure_url").toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
