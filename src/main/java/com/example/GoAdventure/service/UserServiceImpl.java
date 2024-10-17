4b72b80241abb2d07df723c548fdc3337da5b697
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-10-18 00:03:11 +0300

    Added user registration, verification token handling, and email confirmation features.

--- src/main/java/com/example/GoAdventure/service/UserServiceImpl.java
+++ src/main/java/com/example/GoAdventure/service/UserServiceImpl.java
@@ -1,30 +1,79 @@
 package com.example.GoAdventure.service;
 
 import com.example.GoAdventure.config.AdminConfig;
+import com.example.GoAdventure.model.dtos.UserRegisterDTO;
 import com.example.GoAdventure.model.entity.User;
 import com.example.GoAdventure.model.entity.UserRole;
+import com.example.GoAdventure.model.entity.VerificationToken;
 import com.example.GoAdventure.model.enums.Role;
 import com.example.GoAdventure.repository.UserRepository;
+import com.example.GoAdventure.repository.VerificationTokenRepository;
 import com.example.GoAdventure.service.interfaces.UserRoleService;
 import com.example.GoAdventure.service.interfaces.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final AdminConfig adminConfig;
+    private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final UserRoleService userRoleService;
+    private final VerificationTokenRepository tokenRepository;
+    private final JavaMailSender mailSender;
 
-    public UserServiceImpl(AdminConfig adminConfig,PasswordEncoder passwordEncoder, UserRepository userRepository, UserRoleService userRoleService) {
+    @Value("${app.mysql.host}")
+    private String mysqlHost;
+
+    @Value("${app.mysql.port}")
+    private String mysqlPort;
+
+    public UserServiceImpl(AdminConfig adminConfig, ModelMapper modelMapper, PasswordEncoder passwordEncoder, UserRepository userRepository, UserRoleService userRoleService, VerificationTokenRepository tokenRepository, JavaMailSender mailSender) {
         this.adminConfig = adminConfig;
+        this.modelMapper = modelMapper;
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
         this.userRoleService = userRoleService;
+        this.tokenRepository = tokenRepository;
+        this.mailSender = mailSender;
+    }
+
+    @Override
+    public void saveUser(UserRegisterDTO userRegisterDTO) {
+        User user = this.modelMapper.map(userRegisterDTO, User.class);
+        user.setPassword(passwordEncoder.encode(userRegisterDTO.getPassword()));
+        user.getRoles().add(this.userRoleService.findByRole(Role.USER));
+        user.setEnabled(false);
+        this.userRepository.save(user);
+
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken()
+                .setToken(token)
+                .setUser(user)
+                .setExpiryDate(LocalDateTime.now().plusDays(1));
+        tokenRepository.save(verificationToken);
+
+        sendVerificationEmail(user.getEmail(), token);
+    }
+
+
+    private void sendVerificationEmail(String email, String token) {
+        String verificationUrl = "http://" + mysqlHost + ":" + mysqlPort + "/login-register/verification-page?token=" + token;
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Confirm your registration");
+        message.setText("Click the link to confirm your registration: " + verificationUrl);
+        mailSender.send(message);
     }
 
     @Override
@@ -43,5 +92,18 @@ public class UserServiceImpl implements UserService {
                     .setEnabled(true);
             this.userRepository.save(user);
         }
+    }
+
+    @Override
+    public boolean confirmUserAccount(String token) {
+        VerificationToken verificationToken = tokenRepository.findByToken(token);
+
+        if (verificationToken != null && verificationToken.getExpiryDate().isAfter(LocalDateTime.now())) {
+            User user = verificationToken.getUser();
+            user.setEnabled(true);
+            userRepository.save(user);
+            return true;
+        }
+        return false;
     }
 }
