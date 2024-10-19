a516cedfa87f6d29edcd9dc9975f49331e9b973d
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-10-19 07:43:38 +0300

    Added user registration, admin initialization, and account confirmation methods.

--- src/main/java/com/example/GoAdventure/service/interfaces/UserService.java
+++ src/main/java/com/example/GoAdventure/service/interfaces/UserService.java
@@ -1,5 +1,12 @@
 package com.example.GoAdventure.service.interfaces;
 
+import com.example.GoAdventure.model.dtos.UserRegisterDTO;
+
 public interface UserService {
 
+    void saveUser(UserRegisterDTO userRegisterDTO);
+
+    void dbInitAdmin();
+
+    boolean confirmUserAccount(String token);
 }
