6cd3def3d58cc013c99b1de8720496f67db88fd1
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-10-29 00:37:56 +0200

    Refactored options toggle logic.

--- src/main/resources/static/js/dropdownButton.js
+++ src/main/resources/static/js/dropdownButton.js
@@ -1,12 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const toggleButton = document.getElementById("toggleOptions");
-  const optionsContainer = document.getElementById("optionsContainer");
+    const selectBtn = document.getElementById("selectedOption");
+    const optionsContainer = document.getElementById("optionsContainer");
 
-  toggleButton.addEventListener("click", function () {
-    if (optionsContainer.style.display === "flex") {
-      optionsContainer.style.display = "none";
-    } else {
-      optionsContainer.style.display = "flex";
-    }
-  });
+    selectBtn.addEventListener("click", function () {
+        if (optionsContainer.classList.contains('show')) {
+            optionsContainer.classList.remove('show');
+        } else {
+            optionsContainer.classList.add('show');
+        }
+    });
+
+    document.querySelectorAll('.option-link').forEach(option => {
+        option.addEventListener('click', function () {
+            selectBtn.textContent = this.getAttribute('data-value');
+            optionsContainer.classList.remove('show');
+        });
+    });
 });
