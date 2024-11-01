b648c73a20eff4c90bab2487d00f3c62938daf3b
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-11-02 00:31:15 +0200

    Added method to filter adventures by location and type.

--- src/main/java/com/example/GoAdventure/repository/AdventureRepository.java
+++ src/main/java/com/example/GoAdventure/repository/AdventureRepository.java
@@ -2,10 +2,14 @@ package com.example.GoAdventure.repository;
 
 
 import com.example.GoAdventure.model.entity.Adventure;
+import com.example.GoAdventure.model.enums.AdventureType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AdventureRepository extends JpaRepository<Adventure,Long> {
 
+    List<Adventure> findByLocationNameAndType(String locationName, AdventureType adventureType);
 }
