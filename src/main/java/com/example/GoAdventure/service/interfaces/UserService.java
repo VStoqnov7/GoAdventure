afde7f6937367c4ee83a21fedadce206c54e1988
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-11-10 03:09:07 +0200

    Added email lookup and notification methods.

--- src/main/java/com/example/GoAdventure/service/interfaces/UserService.java
+++ src/main/java/com/example/GoAdventure/service/interfaces/UserService.java
@@ -1,6 +1,12 @@
 package com.example.GoAdventure.service.interfaces;
 
 import com.example.GoAdventure.model.dtos.UserRegisterDTO;
+import com.example.GoAdventure.model.entity.Adventure;
+import com.example.GoAdventure.model.entity.PendingBooking;
+import com.example.GoAdventure.model.entity.User;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
 
@@ -9,4 +15,8 @@ public interface UserService {
     void dbInitAdmin();
 
     boolean confirmUserAccount(String token);
+
+    Optional<User> findByEmail(String email);
+
+    void sendNotificationEmail(PendingBooking pendingBooking, boolean isApproved);
 }
