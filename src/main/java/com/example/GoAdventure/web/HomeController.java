abcb6a095e27820b983b881901e6048819722bec
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-11-17 17:17:13 +0200

    Added adventure data loading logic for home page using ModelAttributeUtil with initial, following, and extended lists.

--- src/main/java/com/example/GoAdventure/web/HomeController.java
+++ src/main/java/com/example/GoAdventure/web/HomeController.java
@@ -1,16 +1,30 @@
 package com.example.GoAdventure.web;
 
+import com.example.GoAdventure.model.view.AdventureView;
+import com.example.GoAdventure.service.interfaces.AdventureService;
+import com.example.GoAdventure.util.ModelAttributeUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 
+    private final AdventureService adventureService;
+
+    public HomeController(AdventureService adventureService) {
+        this.adventureService = adventureService;
+    }
+
     @GetMapping
     public ModelAndView home(ModelAndView model){
+        ModelAttributeUtil.addInitialThreeAdventures(adventureService,model);
+        ModelAttributeUtil.addFollowingThreeAdventures(adventureService,model);
+        ModelAttributeUtil.addExtendedTenAdventureList(adventureService,model);
         model.setViewName("home");
         return model;
     }
