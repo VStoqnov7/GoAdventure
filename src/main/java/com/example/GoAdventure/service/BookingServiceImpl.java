668e9b745d7359ed7f531a7563aacf7608c19255
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-11-27 01:12:06 +0200

    Added method for saving pending bookings as confirmed bookings.

--- src/main/java/com/example/GoAdventure/service/BookingServiceImpl.java
+++ src/main/java/com/example/GoAdventure/service/BookingServiceImpl.java
@@ -1,9 +1,26 @@
 package com.example.GoAdventure.service;
 
+import com.example.GoAdventure.model.entity.Booking;
+import com.example.GoAdventure.model.entity.PendingBooking;
+import com.example.GoAdventure.repository.BookingRepository;
 import com.example.GoAdventure.service.interfaces.BookingService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookingServiceImpl implements BookingService {
 
+    private final BookingRepository bookingRepository;
+    private final ModelMapper modelMapper;
+
+    public BookingServiceImpl(BookingRepository bookingRepository, ModelMapper modelMapper) {
+        this.bookingRepository = bookingRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public void addBooking(PendingBooking pendingBooking) {
+        Booking booking = modelMapper.map(pendingBooking, Booking.class);
+        this.bookingRepository.save(booking);
+    }
 }
