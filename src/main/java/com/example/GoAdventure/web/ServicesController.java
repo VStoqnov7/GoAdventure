69f023f40bdfa3ef3e3d43da28c31670c696e49a
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-11-28 00:11:37 +0200

    Added approve and reject booking functionality.

--- src/main/java/com/example/GoAdventure/web/ServicesController.java
+++ src/main/java/com/example/GoAdventure/web/ServicesController.java
@@ -2,15 +2,12 @@ package com.example.GoAdventure.web;
 
 import com.example.GoAdventure.model.dtos.AdventureDTO;
 import com.example.GoAdventure.service.interfaces.AdventureService;
+import com.example.GoAdventure.service.interfaces.PendingBookingService;
 import com.example.GoAdventure.util.ModelAttributeUtil;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -19,9 +16,11 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class ServicesController {
 
     private final AdventureService adventureService;
+    private final PendingBookingService pendingBookingService;
 
-    public ServicesController(AdventureService adventureService) {
+    public ServicesController(AdventureService adventureService, PendingBookingService pendingBookingService) {
         this.adventureService = adventureService;
+        this.pendingBookingService = pendingBookingService;
     }
 
     @ModelAttribute(name = "adventureDTO")
@@ -32,6 +31,7 @@ public class ServicesController {
     @GetMapping
     public ModelAndView showServices(ModelAndView model){
         ModelAttributeUtil.addEnumsToService(model);
+        ModelAttributeUtil.addAllPendingBookings(model,pendingBookingService);
         model.setViewName("services");
         return model;
     }
@@ -49,6 +49,24 @@ public class ServicesController {
         }
 
         this.adventureService.saveAdventure(adventureDTO);
+        model.setViewName("redirect:/services");
+        return model;
+    }
+
+
+    @PostMapping("/approveBooking/{pendingBookingId}")
+    public ModelAndView approveBooking(@PathVariable Long pendingBookingId,
+                                       ModelAndView model){
+        this.adventureService.approveBooking(pendingBookingId);
+        model.setViewName("redirect:/services");
+        return model;
+    }
+
+
+    @PostMapping("/rejectBooking/{pendingBookingId}")
+    public ModelAndView rejectBooking(@PathVariable Long pendingBookingId,
+                                       ModelAndView model){
+        this.adventureService.rejectBooking(pendingBookingId);
         model.setViewName("redirect:/services");
         return model;
     }
