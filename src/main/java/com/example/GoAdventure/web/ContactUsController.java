87c265a36992560b29ca11a7c50c27aa5d405945
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-11-28 00:11:05 +0200

    Updated 'Contact Us' page to display 4 adventures for all users, with details available only to authenticated users.

--- src/main/java/com/example/GoAdventure/web/ContactUsController.java
+++ src/main/java/com/example/GoAdventure/web/ContactUsController.java
@@ -1,5 +1,7 @@
 package com.example.GoAdventure.web;
 
+import com.example.GoAdventure.service.interfaces.AdventureService;
+import com.example.GoAdventure.util.ModelAttributeUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,15 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/contact-us")
 public class ContactUsController {
 
+    private final AdventureService adventureService;
+
+    public ContactUsController(AdventureService adventureService) {
+        this.adventureService = adventureService;
+    }
+
     @GetMapping
     public ModelAndView showGuestPage(ModelAndView model){
+        ModelAttributeUtil.addInitialFourAdventures(adventureService,model);
         model.setViewName("contact-us");
         return model;
     }
