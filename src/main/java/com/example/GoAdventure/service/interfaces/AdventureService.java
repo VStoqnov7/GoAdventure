9e58853c2c02e5fdcce2b72f549460b29886eeb7
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-11-29 01:06:58 +0200

    Added AdventureService interface with methods for managing adventures and handling booking approval/rejection.

--- src/main/java/com/example/GoAdventure/service/interfaces/AdventureService.java
+++ src/main/java/com/example/GoAdventure/service/interfaces/AdventureService.java
@@ -1,5 +1,35 @@
 package com.example.GoAdventure.service.interfaces;
 
+import com.example.GoAdventure.model.dtos.AdventureDTO;
+import com.example.GoAdventure.model.entity.Adventure;
+import com.example.GoAdventure.model.enums.AdventureType;
+import com.example.GoAdventure.model.view.AdventureView;
+
+import java.util.List;
+
 public interface AdventureService {
 
+    void saveAdventure(AdventureDTO adventureDTO);
+
+    List<AdventureView> findAllAdventures();
+
+    List<AdventureView> findAdventuresByLocationNameAndType(String locationName, AdventureType adventureType);
+
+    Adventure findById(Long adventureId);
+
+    AdventureView createAdventureProfileView(Adventure adventure);
+
+    void deleteAdventure(Long id);
+
+    void approveBooking(Long pendingBookingId);
+
+    void rejectBooking(Long pendingBookingId);
+
+    List<AdventureView> getInitialThreeAdventures();
+
+    List<AdventureView> getFollowingThreeAdventures();
+
+    List<AdventureView> getExtendedTenAdventureList();
+
+    List<AdventureView> getInitialFourAdventures();
 }
