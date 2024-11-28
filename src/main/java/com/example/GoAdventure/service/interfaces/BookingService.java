866da44ea1fbecc991d1940e650d4e252e68e12f
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-11-29 01:07:53 +0200

    Added BookingService interface with method for adding pending bookings.

--- src/main/java/com/example/GoAdventure/service/interfaces/BookingService.java
+++ src/main/java/com/example/GoAdventure/service/interfaces/BookingService.java
@@ -1,5 +1,8 @@
 package com.example.GoAdventure.service.interfaces;
 
+import com.example.GoAdventure.model.entity.PendingBooking;
+
 public interface BookingService {
 
+    void addBooking(PendingBooking pendingBooking);
 }
